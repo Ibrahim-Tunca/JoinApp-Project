db164dfe52b46c39aef577acb359e5a8c257e04c
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2026-04-20 21:49:13 +0200

    update Selectfield
    fixed some Designissues from selectfields

--- script/addTask.js
+++ script/addTask.js
@@ -17,7 +17,6 @@ function setPrioToLow(){
     imgMidPrioRef.src = "./img/addTask/prio-medium.svg"
     imgHighPrioRef.src = "./img/addTask/prio-high.svg"
 
-
     priority = "low";
 }
 
@@ -60,3 +59,68 @@ function setPrioToUrgent(){
 
     priority = "urgent";
 }
+
+
+function initCustomSelects() {
+    const customSelects = document.querySelectorAll(".custom-select-addTask");
+
+    customSelects.forEach((selectElement) => {
+        const toggleButton = selectElement.querySelector(".custom-select-toggle-addTask");
+        const label = selectElement.querySelector(".custom-select-label-addTask");
+        const hiddenInput = selectElement.querySelector('input[type="hidden"]');
+        const menu = selectElement.querySelector(".custom-select-menu-addTask");
+        const options = selectElement.querySelectorAll(".custom-select-option-addTask");
+        const placeholder = selectElement.dataset.placeholder;
+
+        selectElement.classList.add("placeholder");
+
+        toggleButton.addEventListener("click", () => {
+            const isOpen = selectElement.classList.contains("open");
+
+            closeAllCustomSelects();
+
+            if (!isOpen) {
+                selectElement.classList.add("open");
+                menu.classList.remove("d_none");
+            }
+        });
+
+        options.forEach((optionButton) => {
+            optionButton.addEventListener("click", () => {
+                const optionValue = optionButton.dataset.value;
+                const optionText = optionButton.textContent;
+
+                hiddenInput.value = optionValue;
+                label.textContent = optionText;
+                selectElement.classList.remove("placeholder");
+                selectElement.classList.remove("open");
+                menu.classList.add("d_none");
+            });
+        });
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!event.target.closest(".custom-select-addTask")) {
+            closeAllCustomSelects();
+        }
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            closeAllCustomSelects();
+        }
+    });
+}
+
+function closeAllCustomSelects() {
+    document.querySelectorAll(".custom-select-addTask").forEach((selectElement) => {
+        selectElement.classList.remove("open");
+
+        const menu = selectElement.querySelector(".custom-select-menu-addTask");
+        if (menu) {
+            menu.classList.add("d_none");
+        }
+    });
+}
+
+initCustomSelects();
