53aea2fb11321f3424200fec821fb9688e2138e1
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2026-04-24 16:10:05 +0200

    update addTask.html
    all input and selectfield are working right now

--- script/addTask.js
+++ script/addTask.js
@@ -1,5 +1,7 @@
 let priority = "";
 
+let subtaskID = 0;
+
 function setPrioToLow(){
     buttonLowPrioRef = document.getElementById("lowPrioID");
     buttonMidPrioRef = document.getElementById("mediumPrioID");
@@ -59,6 +61,138 @@ function setPrioToUrgent(){
 
     priority = "urgent";
 }
+
+
+function showButtons(){
+    const subtasksInputfieldButtonContainerRef = document.getElementById("subtasksInputfieldButtonContainerID");
+    const subtaskValue = document.forms["addTaskForm"]["subtaskform"].value;
+
+    if(subtaskValue != ""){
+        subtasksInputfieldButtonContainerRef.classList.remove("d_none");
+        return;
+    }
+
+    subtasksInputfieldButtonContainerRef.classList.add("d_none");
+    return;
+}
+
+
+function pasteSubtaskUnderInputfield(){
+    const subtasksInputfieldButtonContainerRef = document.getElementById("subtasksInputfieldButtonContainerID");
+    const subtaskValue = document.forms["addTaskForm"]["subtaskform"].value;
+    const createdSubtasksContainer = document.getElementById("createdSubtasksContainerID");
+
+    createdSubtasksContainer.innerHTML +=   `<div class="single-subtask-container" id="subtaskNr${subtaskID}ID">    
+                                                        <span>• ${subtaskValue}</span>  
+                                                        <div class="subtask-button-container">
+                                                            <img 
+                                                                onclick="deleteSingleSubtask(${subtaskID})"
+                                                                class="subtask-trash-icon" 
+                                                                src="./img/trashcan.svg" 
+                                                                alt=""
+                                                                onmouseenter="this.src='./img/addTask/trashcan-blue.svg'"
+                                                                onmouseleave="this.src='./img/trashcan.svg'"
+                                                            >
+                                                            <div class="subtask-separator"></div>
+                                                            <img 
+                                                                onclick="editSingleSubtask(${subtaskID}, '${subtaskValue}')"
+                                                                class="subtask-pencil-icon" 
+                                                                src="./img/pencil.svg" 
+                                                                alt=""
+                                                                onmouseenter="this.src='./img/addTask/pencil-blue.svg'"
+                                                                onmouseleave="this.src='./img/pencil.svg'"
+                                                            >
+                                                        </div>
+                                            </div>`;
+
+    subtaskID++;
+    subtasksInputfieldButtonContainerRef.classList.add("d_none");
+    document.forms["addTaskForm"]["subtaskform"].value = "";
+}
+
+function deleteInputValueSubtask(){
+    const subtasksInputfieldButtonContainerRef = document.getElementById("subtasksInputfieldButtonContainerID");
+    subtasksInputfieldButtonContainerRef.classList.add("d_none");
+    document.forms["addTaskForm"]["subtaskform"].value = "";
+}
+
+function deleteSingleSubtask(id){
+    const subTaskRef = document.getElementById("subtaskNr" + id + "ID");
+    subTaskRef.remove();
+}
+
+function editSingleSubtask(id, value){
+    const subTaskRef = document.getElementById("subtaskNr" + id + "ID");
+    subTaskRef.style.paddingLeft = "0";
+
+    subTaskRef.innerHTML = `<div class="wrapper-div-container">
+                                <input 
+                                    class="edit-input-subtask-addTask" 
+                                    type="text" 
+                                    placeholder="add new subtask" 
+                                    id="editSubtask${id}">
+                            
+                                <div class="cancel-and-confirm-container">
+                                    <img onclick="deleteSingleSubtask(${id})" class="cancel-icon" src="./img/trashcan.svg" alt="">
+                                    <span class="cancel-and-confirm-separator"></span>
+                                    <img onclick="confirmEditSubtask(${id})" class="confirm-icon" src="./img/addTask/confirm.svg" alt="">
+                                </div>
+                            </div>`
+
+        const inputRef = document.getElementById("editSubtask" + id);
+        inputRef.value = value;
+        inputRef.focus();
+        inputRef.setSelectionRange(value.length, value.length);
+}
+
+function confirmEditSubtask(id){
+    const subTaskRef = document.getElementById("subtaskNr" + id + "ID");
+    const subTaskValue = document.getElementById("editSubtask" + id).value;
+
+
+    subTaskRef.innerHTML =  `
+                                            <div class="single-subtask-container" id="subtaskNr${id}ID">    
+                                                        <span>• ${subTaskValue}</span>  
+                                                        <div class="subtask-button-container">
+                                                            <img 
+                                                                onclick="deleteSingleSubtask(${id})"
+                                                                class="subtask-trash-icon" 
+                                                                src="./img/trashcan.svg" 
+                                                                alt=""
+                                                                onmouseenter="this.src='./img/addTask/trashcan-blue.svg'"
+                                                                onmouseleave="this.src='./img/trashcan.svg'"
+                                                            >
+                                                            <div class="subtask-separator"></div>
+                                                            <img 
+                                                                onclick="editSingleSubtask(${id}, '${subTaskValue}')"
+                                                                class="subtask-pencil-icon" 
+                                                                src="./img/pencil.svg" 
+                                                                alt=""
+                                                                onmouseenter="this.src='./img/addTask/pencil-blue.svg'"
+                                                                onmouseleave="this.src='./img/pencil.svg'"
+                                                            >
+                                                        </div>
+                                            </div>
+                                `;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function initCustomSelects() {
