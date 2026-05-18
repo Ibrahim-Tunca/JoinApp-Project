25a90477f3223cf9b3d4d0aea9df4099d0a616d1
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2026-05-18 14:48:50 +0200

    sort code

---
+++ script/board-special-functions/board-cards.js
@@ -0,0 +1,82 @@
+function getImgByPriority(priority, id){
+    const priorityRef = document.getElementById("priorityNr" + id);
+
+    if (priority === "low") {
+        priorityRef.innerHTML = `<img class="other-prio-icon-card" src="./img/addTask/prio-low.svg">`
+    }
+    if (priority === "medium") {
+        priorityRef.innerHTML = `<img class="mid-prio-icon-card" src="./img/addTask/prio-medium.svg">`
+    }
+    if (priority === "urgent") {
+        priorityRef.innerHTML = `<img class="other-prio-icon-card" src="./img/addTask/prio-high.svg">`
+    }
+}
+
+
+function getHeadlineCardColor(category, id){
+    const categoryRef = document.getElementById("categoryNr" + id);
+
+    if(category === "User Story"){
+        categoryRef.classList.add("background-color-orange");
+    }
+    if(category === "Technical Task"){
+        categoryRef.classList.add("background-color-blue");
+    }
+}
+
+
+function countFinishedSubtasks(subtasks){
+    let subtaskCounter = 0;
+    for (let index = 0; index < subtasks.length; index++) {
+        const currentSubtaskStatus = subtasks[index].status;
+        if(currentSubtaskStatus == true){
+            subtaskCounter++
+        }
+    }
+    return subtaskCounter;
+}
+
+
+function showSubtaskProgress(subtasks, id){
+    const subTaskContainerRef = document.getElementById("subTaskContainerNr" + id);
+    const finishedSubTasks = countFinishedSubtasks(subtasks);
+
+    if(subtasks.length < 1){
+        return;
+    }
+
+    subTaskContainerRef.innerHTML = `
+                                        <progress class="progress-bar-style-board" value="${finishedSubTasks}" max="${subtasks.length}"></progress>
+                                        <span >${finishedSubTasks}/${subtasks.length} Subtasks</span>
+                                    `
+    return;
+}
+
+
+function getLetterBalls(contacts, id){
+    const letterBallContainerRef = document.getElementById("letterBallContainerNr" + id);
+    let distanceBetweenInitalBalls = 0;
+
+    for (let index = 0; index < contacts.length; index++) {
+        const initals = contacts[index].initals;
+        const color = contacts[index].color;
+        const contactId = contacts[index].id;
+
+        letterBallContainerRef.innerHTML += `<div class="letter-ball-card ${color}" style="left: ${distanceBetweenInitalBalls}px;">${initals}</div>`;
+        distanceBetweenInitalBalls = distanceBetweenInitalBalls + 26;
+    }
+}
+
+
+async function getContactsContainerFromTaskByID(id){
+    const taskEntries = await getTaskEntriesFromDataBase();
+
+    for (let index = 0; index < taskEntries.length; index++) {
+        const taskID = taskEntries[index][0];
+        if(taskID === id){
+            const contacts = taskEntries[index][1].contacts;
+            return contacts
+        }
+    }
+    return;
+}
