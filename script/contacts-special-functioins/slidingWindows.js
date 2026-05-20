b81ef25a1d8389c409ea3bcbd8d627834109e08b
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2026-05-20 22:45:13 +0200

    designfixes

--- script/contacts-special-functioins/slidingWindows.js
+++ script/contacts-special-functioins/slidingWindows.js
@@ -1,10 +1,13 @@
 function hideAddContactAndEditContactWindow(){
-    let buttonRef = document.getElementById("contactsButtonID");
-    let overlayRef = document.getElementById("overlayID");
-    let addContactRef = document.getElementById("addContactID");
-    let editContactRef = document.getElementById("editContactID");
+    const buttonRef = document.getElementById("contactsButtonID");
+    const personIconRef = document.getElementById("addPersonIconID");
+    const overlayRef = document.getElementById("overlayID");
+    const addContactRef = document.getElementById("addContactID");
+    const editContactRef = document.getElementById("editContactID");
+
 
     buttonRef.classList.remove("toggle-color-contacts-button");
+    personIconRef.src="./img/contacts/person_add.svg";
     overlayRef.classList.remove("hide-and-show-overlay-contacts");
     addContactRef.classList.add("toggle-addContact-card");
     editContactRef.classList.add("toggle-editContact-card");
@@ -12,20 +15,22 @@ function hideAddContactAndEditContactWindow(){
 
 
 function showAddContactWindow(){
-    let buttonRef = document.getElementById("contactsButtonID");
-    let overlayRef = document.getElementById("overlayID");
-    let contentRef = document.getElementById("addContactID");
+    const buttonRef = document.getElementById("contactsButtonID");
+    const personIconRef = document.getElementById("addPersonIconID");
+    const overlayRef = document.getElementById("overlayID");
+    const contentRef = document.getElementById("addContactID");
 
     contentRef.innerHTML = addContactTemplate(); 
 
     buttonRef.classList.add("toggle-color-contacts-button");
+    personIconRef.src="./img/contacts/person_add-blue.svg";
     overlayRef.classList.add("hide-and-show-overlay-contacts");
     contentRef.classList.remove("toggle-addContact-card");
 }
 
 function showEditContactWindow(name, mail, initials, color, phone, id){
-    let overlayRef = document.getElementById("overlayID");
-    let contentRef = document.getElementById("editContactID");
+    const overlayRef = document.getElementById("overlayID");
+    const contentRef = document.getElementById("editContactID");
 
     contentRef.innerHTML = editContactTemplate(initials, color, id);
     
