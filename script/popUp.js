5997ad2cbdc76b430225914851ad2f90aeab327b
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2026-06-23 14:53:51 +0200

    fix popupwindow
    the user can the window close when he click somewhrere outside the window

--- script/popUp.js
+++ script/popUp.js
@@ -1,7 +1,21 @@
-function togglePopUpMenue(){
-    contentRef = document.getElementById("popUpID");
-    contentRef.classList.toggle("d_none");
+function togglePopUpMenue(event) {
+    event.stopPropagation();
+
+    const popup = document.getElementById("popUpID");
+    popup.classList.toggle("d_none");
 }
+
+document.addEventListener("click", function (event) {
+    const popup = document.getElementById("popUpID");
+    const button = document.getElementById("userInitialButtonID");
+
+    const clickInsidePopup = popup.contains(event.target);
+    const clickOnButton = button.contains(event.target);
+
+    if (!clickInsidePopup && !clickOnButton) {
+        popup.classList.add("d_none");
+    }
+});
 
 
 function logout(){
